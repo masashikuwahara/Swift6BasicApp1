e749b92a5ee2a6288e2b989e115bde73a7c96c0b
桑原雅史 <brucemclarenp4@gmail.com>
2021-06-06 12:27:10 +0900

    first commit

---
+++ Swift6BasicApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6BasicApp1
+//
+//  Created by 桑原雅史 on 2021/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
